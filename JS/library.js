1740321d8f99b1275fc3ad81c0b077bf8fe6ddb5
Lyle Scott <Lyle.C.Scott@gmail.com>
2015-08-03 20:25:49 -0700

    Local storage active, app working.

--- JS/library.js
+++ JS/library.js
@@ -45,30 +45,46 @@ Shelf.prototype.removeBook = function (oldBook) {
   }
 };
 
-var campBecca = new Library("Camp Becca");
-var philosophy = new Shelf("Philosophy");
-var dogStories = new Shelf("Books about dogs");
-var kant1 = new Book("Critique of Pure Reason", "Immanuel Kant");
-var hegel1 = new Book("Phenomenology of Spirit", "Georg Hegel");
-var yeller = new Book("Old Yeller", "Fred Gipson");
-var lassie = new Book("Lassie Come-Home", "Eric Knight");
+var save = function() {
+    var session = JSON.stringify(campBecca);
+    localStorage.campBecca = session;
+};
 
 var render = function() {
-  $('#library').children().remove();
+  $('#library').empty();
   $.each(campBecca.shelf, function(i, val) {
     $('#library').append('<li class=\"shelf\" id=\"campBecca' + i + '\"><h2>' + campBecca.shelf[i].subject + '</h2></li>');
     $.each(campBecca.shelf[i].book, function(j, val) {
       $('#campBecca' + i).append('<h3 class\"book\">\"' + campBecca.shelf[i].book[j].title + '\" by ' + campBecca.shelf[i].book[j].author + '</h3>')
     })
   })
+  save();
 };
-  
-campBecca.addShelf(philosophy);
-campBecca.addShelf(dogStories);
-philosophy.addBook(kant1);
-philosophy.addBook(hegel1);
-dogStories.addBook(yeller);
-dogStories.addBook(lassie);
+
+if (localStorage.campBecca) {
+  var campBecca = JSON.parse(localStorage.campBecca);
+} else {
+  var campBecca = new Library("Camp Becca");
+  var philosophy = new Shelf("Philosophy");
+  var dogStories = new Shelf("Books about dogs");
+  var plush = new Shelf("Stuffed animals");
+  var kant1 = new Book("Critique of Pure Reason", "Immanuel Kant");
+  var hegel1 = new Book("Phenomenology of Spirit", "Georg Hegel");
+  var yeller = new Book("Old Yeller", "Fred Gipson");
+  var lassie = new Book("Lassie Come-Home", "Eric Knight");
+  var pooh = new Book("Winnie-The-Pooh", "A. A. Milne");
+  var caterpillar = new Book("The Very Hungry Caterpillar", "Eric Carle");
+
+  campBecca.addShelf(philosophy);
+  campBecca.addShelf(dogStories);
+  campBecca.addShelf(plush);
+  philosophy.addBook(kant1);
+  philosophy.addBook(hegel1);
+  dogStories.addBook(yeller);
+  dogStories.addBook(lassie);
+  plush.addBook(pooh);
+  plush.addBook(caterpillar);  
+};
 
 render();
 console.log("done");
