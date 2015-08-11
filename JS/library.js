2b095f55e9d26d1b369924e552814d017745853e
Lyle Scott <Lyle.C.Scott@gmail.com>
2015-08-11 02:21:40 -0700

    Restructuring the entire fkg thing to make it make more sense from the ground up.

--- JS/library.js
+++ JS/library.js
@@ -1,64 +1,94 @@
 'use strict'
 
 var Library = function(branch) {
-this.branch = branch;
-this.shelf = [];
+  this.branch = branch;
+  this.shelf = [];
+  this.index = [];
 };
 
 var Shelf = function(subject) {
-this.subject = subject;
-this.book = [];
+  this.subject = subject;
+  this.book = [];
 };
 
-var Book = function(title, author) {
+var Book = function(ID, title, author) {
+  this.ID = ID;
   this.title = title;
   this.author = author;
+  this.location = unshelved;
 };
 
-Book.prototype.shelve = function(place) {
-  place.book.push(this);
+Shelf.prototype.addToLibrary = function (newLibrary) {
+  newLibrary.shelf.push(this);
   render();
 };
 
-Book.prototype.unshelve = function(place) {
-  for (var i =0; i < place.book.length; i++) {
-    if (this == place.book[i]) {
-      place.book.splice(i,1);
-      render();
-      break;
+Shelf.prototype.removeFromLibrary = function (oldLibrary) {
+  var i = this;
+  $.each(oldLibrary.shelf, function(j) {
+    if (i === this) {
+      oldLibrary.shelf.splice(j,1);
+      render(); 
     }
-  }
+    return(i !== this);
+  })
 };
 
-Library.prototype.addShelf = function (newShelf) {
-  this.shelf.push(newShelf);
+Book.prototype.shelve = function(shelf) {
+  var i = this;
+  $.each(shelf.book, function(j) {
+    if (i === this) {
+      console.log("Book is already in this shelf.")
+      return(false);
+    }
+  })
+  shelf.book.push(this);
+    // needs loop to check if is already in library
+  campBecca.index.push(this);
+  this.location = shelf;
+  console.log(i.title, "is on shelf:", shelf.subject);
+  var temp = [];
+  $.each(campBecca.index, function (j) {
+    temp.push(campBecca.index[j].title);
+  });
+  console.log("Library index: ", temp)
   render();
 };
 
-Library.prototype.removeShelf = function (oldShelf) {
-  for (var i = 0; i < this.shelf.length; i++) {
-    if (oldShelf == this.shelf[i].subject) {
-      this.shelf.splice(i,1);
-      render();
-      break;
+Book.prototype.unshelve = function () {
+  var i = this;
+    $.each(unshelved.book, function(j) {
+    if (i === this) {
+      console.log("Book is already unshelved.")
+      return(false);
     }
-  }
+  })
+  unshelved.book.push(this);
+  // needs loop to check if is already in library
+  campBecca.index.push(this);
+  this.location = unshelved;
+  console.log(i.title, "is unshelved.");
+  var temp = [];
+  $.each(campBecca.index, function (j) {
+    temp.push(campBecca.index[j].title);
+  });
+  console.log("Library index: ", temp)
+  render();
 };
 
-Shelf.prototype.addBook = function (newBook) {
-  this.book.push(newBook)
-      render();
-};
 
-Shelf.prototype.removeBook = function (oldBook) {
-  for (var i = 0; i < this.book.length; i++) {
-    if (oldBook == this.book[i].title) {
-      this.book.splice(i,1);
-      render();
-      break;
-    }
-  }
-};
+// Book.prototype.unshelve = function() {
+//   var i = this; 
+//   $.each(book.location.book, function (j) {
+//     if (i === this) {
+//       shelf00.book.push(this);
+//       this.location = shelf00;
+//       shelf.book.splice(j,1);
+//       render();
+//     }
+//     return(i !== this);
+//   })
+// };
 
 // THIS LOCAL STORAGE APPROACH DOESN'T WORK. NEED SOMETHING MORE SOPHISTICATED.
 // var save = function() {
@@ -75,7 +105,7 @@ var render = function() {
   $.each(campBecca.shelf, function(i, val) {
     $('#library').append('<li class=\"shelf\" id=\"campBecca' + i + '\"><h2>' + campBecca.shelf[i].subject + '</h2></li>');
     $.each(campBecca.shelf[i].book, function(j, val) {
-      $('#campBecca' + i).append('<h3 class\"book\">\"' + campBecca.shelf[i].book[j].title + '\" by ' + campBecca.shelf[i].book[j].author + '</h3>')
+      $('#campBecca' + i).append('<h3 class\"book\">\"' + campBecca.shelf[i].book[j].title + '\" - ' + campBecca.shelf[i].book[j].author + '</h3>')
     })
   })
   // save()
@@ -85,26 +115,26 @@ var render = function() {
 //   var campBecca = JSON.parse(localStorage.campBecca);
 // } else {
   var campBecca = new Library("Camp Becca");
-  var philosophy = new Shelf("Philosophy");
-  var dogStories = new Shelf("Books about dogs");
-  var plush = new Shelf("Stuffed animals");
-  var kant1 = new Book("Critique of Pure Reason", "Immanuel Kant");
-  var hegel1 = new Book("Phenomenology of Spirit", "Georg Hegel");
-  var yeller = new Book("Old Yeller", "Fred Gipson");
-  var lassie = new Book("Lassie Come-Home", "Eric Knight");
-  var pooh = new Book("Winnie-The-Pooh", "A. A. Milne");
-  var caterpillar = new Book("The Very Hungry Caterpillar", "Eric Carle");
+  var unshelved = new Shelf("Unshelved");
+  var shelf01 = new Shelf("Philosophy");
+  var shelf02 = new Shelf("Books about dogs");
+  var shelf03 = new Shelf("Stuffed animals");
+  var book01 = new Book(1, "Critique of Pure Reason", "Immanuel Kant");
+  var book02 = new Book(2, "Phenomenology of Spirit", "Georg Hegel");
+  var book03 = new Book(3, "Old Yeller", "Fred Gipson");
+  var book04 = new Book(4, "Lassie Come-Home", "Eric Knight");
+  var book05 = new Book(5, "Winnie-The-Pooh", "A. A. Milne");
+  var book06 = new Book(6, "The Very Hungry Caterpillar", "Eric Carle");
 
-  campBecca.addShelf(philosophy);
-  campBecca.addShelf(dogStories);
-  campBecca.addShelf(plush);
-  philosophy.addBook(kant1);
-  philosophy.addBook(hegel1);
-  dogStories.addBook(yeller);
-  dogStories.addBook(lassie);
-  plush.addBook(pooh);
-  plush.addBook(caterpillar);  
-// };
+  shelf01.addToLibrary(campBecca);
+  shelf02.addToLibrary(campBecca);
+  shelf03.addToLibrary(campBecca);
+  book01.shelve(shelf01);
+  book02.shelve(shelf01);
+  book03.shelve(shelf02);
+  book04.shelve(shelf02);
+  book05.shelve(shelf03);
+  book06.shelve(shelf03);
 
 render();
 console.log("done");
@@ -113,7 +143,7 @@ $('#add-shelf').on({'click': function() {
   if ($('#subject').val() !== "") {
     console.log($('#subject').val());
     var userShelf = new Shelf($('#subject').val());
-    campBecca.addShelf(userShelf);
+    userShelf.addToLibrary(campBecca);
     render();
   }
 }});
@@ -121,14 +151,16 @@ $('#add-shelf').on({'click': function() {
 $('#remove-shelf').on({'click': function() {
   if ($('#subject').val() !== "") {
     console.log($('#subject').val());
-    campBecca.removeShelf($('#subject').val());
+    var userShelf = $('#subject').val();
+    // Needs VARIABLE NAME CONCATENATION
+    userShelf.removeFromLibrary(campBecca);
     render();
   }
 }});
 
   $('#add-book').on({'click': function() {
   console.log(!(($('#title').val()) == "" && ($('author').val() == "")));
-  console.log($("input:radio[name=shelf]").val() !== "");
+  // console.log($("input:radio[name=shelf]").val() !== "");
   //   if (!(($('#title').val()) == "" && ($('author').val() == "")) && $("input:radio[name=shelf]").val() !== "") {
   //     $("input:radio[name=shelf]").on.click(function() {
   //       console.log($(this).val());
@@ -139,24 +171,24 @@ $('#remove-shelf').on({'click': function() {
   //       destinationShelf.addBook(userBook);
   //       render();
   //     }
-  //   )}
-  // }});
+    //   )
+    // }
+  }});
 
 // this works for selecting items with the radio buttons.
-  $("input:radio[name=shelf]").on({'click': function() {
-    var destinationShelf = $(this).val();
-    console.log(destinationShelf);
-    if (!(($('#title').val()) == "" && ($('#author').val() == ""))) {
-      console.log("First pass");
-      $('#add-book').on({'click': function() {
-        var userBook = new Book($('#title').val(), $('#author').val());
-        console.log(userBook);
-        userBook.shelve($("input:radio[name=shelf]"))
-        render();
-      }})
-    }
+$("input:radio[name=shelf]").on({'click': function() {
+  destinationShelf = $(this).val();
+  console.log(destinationShelf);
+  if (!(($('#title').val()) == "" && ($('#author').val() == ""))) {
+    console.log("First pass");
+    $('#add-book').on({'click': function() {
+      userBook = new Book($('#title').val(), $('#author').val());
+      console.log(userBook);
+      userBook.shelve($("input:radio[name=shelf]"))
+      render();
+    }})
   }
-});
+}});
 
 
 //   if (!(($('#title').val()) == "" && ($('author').val() == "")) && $("input:radio[name=shelf]").val() !== "") {
