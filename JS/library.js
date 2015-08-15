7901fd98295aecbc0852fe1f37cdbf8c8cb047b1
Lyle Scott <Lyle.C.Scott@gmail.com>
2015-08-15 02:33:07 -0700

    Erase and empty functions work

--- JS/library.js
+++ JS/library.js
@@ -1,6 +1,7 @@
 'use strict'
 
-var unsorted = {title:"unsorted", type:"storage", ID:0, index:[]};
+var unsorted = {title:"unsorted", type:"storage", ID:-1, index:[]};
+var trash = {title:"trash", type: "storage", ID:-2, index:[]};
 
 var Library = function(title) {
   this.type = "library";
@@ -22,39 +23,39 @@ var checkIn = function (libraryItem, branch) {
   unsorted.index.push(libraryItem);
 };
 
-LibraryItem.prototype.AssignBook = function (title, author) {
+LibraryItem.prototype.AssignBook = function (title, author, branch) {
   this.type = "book";
   this.title = title;
   this.author = author;
-  checkIn(this);
+  checkIn(this, branch);
 };
 
-LibraryItem.prototype.AssignMagazine = function (title, month, year) {
+LibraryItem.prototype.AssignMagazine = function (title, month, year, branch) {
   this.type = "magazine";
   this.title = title;
   this.month = month;
   this.year = year;
-  checkIn(this);
+  checkIn(this, branch);
 }
 
-LibraryItem.prototype.AssignCD = function (title, artist) {
+LibraryItem.prototype.AssignCD = function (title, artist, branch) {
   this.type = "CD";
   this.title = title;
-  checkIn(this);
+  checkIn(this, branch);
 }
 
-LibraryItem.prototype.AssignShelf = function (title) {
+LibraryItem.prototype.AssignShelf = function (title, branch) {
   this.type = "shelf";
   this.title = title;
   this.index = [];
-  checkIn(this);
+  checkIn(this, branch);
 };
 
-LibraryItem.prototype.AssignBox = function (title) {
+LibraryItem.prototype.AssignBox = function (title, branch) {
   this.type = "box";
   this.title = title;
   this.index = [];
-  checkIn(this);
+  checkIn(this, branch);
 }
 
 LibraryItem.prototype.Add = function (destination) {
@@ -84,8 +85,29 @@ LibraryItem.prototype.Add = function (destination) {
   }
 };
 
-LibraryItem.prototype.Remove = function () {
+LibraryItem.prototype.Remove = function() {
 this.Add(unsorted);
+};
+
+LibraryItem.prototype.Empty = function() {
+  if (this.index) {
+    for (var i = this.index.length-1; i >= 0; i--) {
+      this.index[i].Add(unsorted);
+    }
+  }
+}; 
+
+LibraryItem.prototype.Erase = function () {
+  if (this.index) {
+    this.Empty();
+  }
+  this.Add(trash);
+  trash.index = [];
+  this.ID = null;
+  this.location = null;
+  this.branch = null;
+  this.title = null;
+  this.type = null;
 };
 
 var render = function() {
@@ -120,22 +142,22 @@ var CD02 = new LibraryItem();
 var box01 = new LibraryItem();
 var box02 = new LibraryItem();
 
-shelf01.AssignShelf("Philosophy");
-shelf02.AssignShelf("Books about dogs");
-shelf03.AssignShelf("Stuffed animals");
-shelf04.AssignShelf("Junk shelf");
-book01.AssignBook("Critique of Pure Reason", "Immanuel Kant");
-book02.AssignBook("Phenomenology of Spirit", "Georg Hegel");
-book03.AssignBook("Old Yeller", "Fred Gipson");
-book04.AssignBook("Lassie Come-Home", "Eric Knight");
-book05.AssignBook("Winnie-The-Pooh", "A. A. Milne");
-book06.AssignBook("The Very Hungry Caterpillar", "Eric Carle");
-mag01.AssignMagazine("Muffintop Monthly", 12, 2004);
-mag02.AssignMagazine("Games Magazine", 3, 1985);
-CD01.AssignCD("Huey Lewis and the News", "Sports");
-CD02.AssignCD("The Beatles", "The White Album");
-box01.AssignBox("periodicals");
-box02.AssignBox("media");
+shelf01.AssignShelf("Philosophy", campBecca);
+shelf02.AssignShelf("Books about dogs", campBecca);
+shelf03.AssignShelf("Stuffed animals", campBecca);
+shelf04.AssignShelf("Junk shelf", campBecca);
+book01.AssignBook("Critique of Pure Reason", "Immanuel Kant", campBecca);
+book02.AssignBook("Phenomenology of Spirit", "Georg Hegel", campBecca);
+book03.AssignBook("Old Yeller", "Fred Gipson", campBecca);
+book04.AssignBook("Lassie Come-Home", "Eric Knight", campBecca);
+book05.AssignBook("Winnie-The-Pooh", "A. A. Milne", campBecca);
+book06.AssignBook("The Very Hungry Caterpillar", "Eric Carle", campBecca);
+mag01.AssignMagazine("Muffintop Monthly", 12, 2004, campBecca);
+mag02.AssignMagazine("Games Magazine", 3, 1985, campBecca);
+CD01.AssignCD("Huey Lewis and the News", "Sports", campBecca);
+CD02.AssignCD("The Beatles", "The White Album", campBecca);
+box01.AssignBox("periodicals", campBecca);
+box02.AssignBox("media", campBecca);
 
 shelf01.Add(campBecca);
 shelf02.Add(campBecca);
