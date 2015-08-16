24b167634e7569e5b21ea935a66c8f59c8f08a2d
Lyle Scott <Lyle.C.Scott@gmail.com>
2015-08-16 14:51:06 -0700

    removed a typo.

--- JS/library.js
+++ JS/library.js
@@ -4,7 +4,7 @@ var unsorted = {title:"unsorted", type:"storage", ID:-1, index:[]};
 var trash = {title:"trash", type: "storage", ID:-2, index:[]};
 
 var Library = function(title) {
-  this.type = "library";
+  this.type = "branch";
   this.title = title;
   this.index = [];
   this.IDList = [];
@@ -16,49 +16,49 @@ var LibraryItem = function(branch) {
   this.location = unsorted;
 };
 
-var checkIn = function (libraryItem, branch) { 
+var CheckIn = function (libraryItem, branch) { 
   libraryItem.ID = (branch.IDList.length);
   LibraryItem.branch = branch;
   branch.IDList.push(libraryItem);
   unsorted.index.push(libraryItem);
 };
 
-LibraryItem.prototype.AssignBook = function (title, author, branch) {
+LibraryItem.prototype.assignBook = function (title, author, branch) {
   this.type = "book";
   this.title = title;
   this.author = author;
-  checkIn(this, branch);
+  CheckIn(this, branch);
 };
 
-LibraryItem.prototype.AssignMagazine = function (title, month, year, branch) {
+LibraryItem.prototype.assignMagazine = function (title, month, year, branch) {
   this.type = "magazine";
   this.title = title;
   this.month = month;
   this.year = year;
-  checkIn(this, branch);
+  CheckIn(this, branch);
 }
 
-LibraryItem.prototype.AssignCD = function (title, artist, branch) {
+LibraryItem.prototype.assignCD = function (title, artist, branch) {
   this.type = "CD";
   this.title = title;
-  checkIn(this, branch);
+  CheckIn(this, branch);
 }
 
-LibraryItem.prototype.AssignShelf = function (title, branch) {
+LibraryItem.prototype.assignShelf = function (title, branch) {
   this.type = "shelf";
   this.title = title;
   this.index = [];
-  checkIn(this, branch);
+  CheckIn(this, branch);
 };
 
-LibraryItem.prototype.AssignBox = function (title, branch) {
+LibraryItem.prototype.assignBox = function (title, branch) {
   this.type = "box";
   this.title = title;
   this.index = [];
-  checkIn(this, branch);
+  CheckIn(this, branch);
 }
 
-LibraryItem.prototype.Add = function (destination) {
+LibraryItem.prototype.add = function (destination) {
   var isCompleted = false;
   if (destination.index == null) {
     console.log(destination.type, ": ", destination.title, " does not contain storage.");
@@ -85,19 +85,19 @@ LibraryItem.prototype.Add = function (destination) {
   }
 };
 
-LibraryItem.prototype.Remove = function() {
+LibraryItem.prototype.remove = function() {
 this.Add(unsorted);
 };
 
-LibraryItem.prototype.Empty = function() {
+LibraryItem.prototype.empty = function() {
   if (this.index) {
     for (var i = this.index.length-1; i >= 0; i--) {
-      this.index[i].Add(unsorted);
+      this.index[i].add(unsorted);
     }
   }
 }; 
 
-LibraryItem.prototype.Erase = function () {
+LibraryItem.prototype.erase = function() {
   if (this.index) {
     this.Empty();
   }
@@ -142,39 +142,39 @@ var CD02 = new LibraryItem();
 var box01 = new LibraryItem();
 var box02 = new LibraryItem();
 
-shelf01.AssignShelf("Philosophy", campBecca);
-shelf02.AssignShelf("Books about dogs", campBecca);
-shelf03.AssignShelf("Stuffed animals", campBecca);
-shelf04.AssignShelf("Junk shelf", campBecca);
-book01.AssignBook("Critique of Pure Reason", "Immanuel Kant", campBecca);
-book02.AssignBook("Phenomenology of Spirit", "Georg Hegel", campBecca);
-book03.AssignBook("Old Yeller", "Fred Gipson", campBecca);
-book04.AssignBook("Lassie Come-Home", "Eric Knight", campBecca);
-book05.AssignBook("Winnie-The-Pooh", "A. A. Milne", campBecca);
-book06.AssignBook("The Very Hungry Caterpillar", "Eric Carle", campBecca);
-mag01.AssignMagazine("Muffintop Monthly", 12, 2004, campBecca);
-mag02.AssignMagazine("Games Magazine", 3, 1985, campBecca);
-CD01.AssignCD("Huey Lewis and the News", "Sports", campBecca);
-CD02.AssignCD("The Beatles", "The White Album", campBecca);
-box01.AssignBox("periodicals", campBecca);
-box02.AssignBox("media", campBecca);
+shelf01.assignShelf("Philosophy", campBecca);
+shelf02.assignShelf("Books about dogs", campBecca);
+shelf03.assignShelf("Stuffed animals", campBecca);
+shelf04.assignShelf("Junk shelf", campBecca);
+book01.assignBook("Critique of Pure Reason", "Immanuel Kant", campBecca);
+book02.assignBook("Phenomenology of Spirit", "Georg Hegel", campBecca);
+book03.assignBook("Old Yeller", "Fred Gipson", campBecca);
+book04.assignBook("Lassie Come-Home", "Eric Knight", campBecca);
+book05.assignBook("Winnie-The-Pooh", "A. A. Milne", campBecca);
+book06.assignBook("The Very Hungry Caterpillar", "Eric Carle", campBecca);
+mag01.assignMagazine("Muffintop Monthly", 12, 2004, campBecca);
+mag02.assignMagazine("Games Magazine", 3, 1985, campBecca);
+CD01.assignCD("Huey Lewis and the News", "Sports", campBecca);
+CD02.assignCD("The Beatles", "The White Album", campBecca);
+box01.assignBox("periodicals", campBecca);
+box02.assignBox("media", campBecca);
 
-shelf01.Add(campBecca);
-shelf02.Add(campBecca);
-shelf03.Add(campBecca);
-shelf04.Add(campBecca);
-book01.Add(shelf01);
-book02.Add(shelf01);
-book03.Add(shelf02);
-book04.Add(shelf02);
-book05.Add(shelf03);
-book06.Add(shelf03);
-CD01.Add(box02);
-CD02.Add(box02);
-mag01.Add(box01);
-mag02.Add(box01);
-box01.Add(shelf04);
-box02.Add(shelf04);
+shelf01.add(campBecca);
+shelf02.add(campBecca);
+shelf03.add(campBecca);
+shelf04.add(campBecca);
+book01.add(shelf01);
+book02.add(shelf01);
+book03.add(shelf02);
+book04.add(shelf02);
+book05.add(shelf03);
+book06.add(shelf03);
+CD01.add(box02);
+CD02.add(box02);
+mag01.add(box01);
+mag02.add(box01);
+box01.add(shelf04);
+box02.add(shelf04);
 
 render();
 
